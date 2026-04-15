c516635c7ee47e7a75f4e591da58ef2f69c1f484
Hariom Kumar <hariomgok@gmail.com>
2026-04-15 21:54:09 +0530

    Add text block folder that means when need same format of text priting than use triple double inverted comma

---
+++ Text_Block/TextBlockDemo.java
@@ -0,0 +1,15 @@
+class TextBlockDemo
+{
+	public static void main(String args[])
+	{
+		String str = """
+	MIT, Meerut 
+Department of CSE 
+		CS-4D
+			Marks List """;
+		System.out.println(str);
+
+	}
+
+
+}
