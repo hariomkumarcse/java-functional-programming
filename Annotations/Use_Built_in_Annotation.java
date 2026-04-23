8dfbca294389a6e4c5e219e0785266cce3ce2a9e
Hariom Kumar <hariomgok@gmail.com>
2026-04-23 22:57:59 +0530

    In Annotations folder add common use of built-in annotations and with lambda expression use

--- Annotations/Use_Built_in_Annotation.java
+++ Annotations/Use_Built_in_Annotation.java
@@ -19,3 +19,26 @@ void oldMethod()
 	System.out.println("This method is deprecated");
 } 
 }
+
+class Child extends Parent 
+{
+	@Override
+	void display()
+	{
+		System.out.println("Child display method");
+	}
+}
+class Use_Built_in_Annotation
+{
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args)
+	{
+		MyInterface obj = () -> 
+			System.out.println("Hello from Lambda!");
+			obj.show();
+	
+			Child c = new Child();
+			c.display();
+			c.oldMethod();
+	}
+} 
